6a8aa916c842d7e7057908546c4377894631e4d4
Antonio Jurić <juricantonio18@gmail.com>
2024-08-02 15:03:30 +0200

    Finished balance

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,26 @@ import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const transactions = ref([
   { id: 1, text: "Car", amount: 200 },
-  { id: 2, text: "Car", amount: -200 },
+  { id: 2, text: "Car", amount: 200 },
+  { id: 2, text: "Car", amount: -33 },
 ]);
+
+const total = computed(() => {
+  return transactions.value.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
+});
 </script>
 
 <template>
   <Header></Header>
   <div class="container">
-    <Balance></Balance>
+    <Balance :total="total"></Balance>
     <IncomeExpenses></IncomeExpenses>
     <TransactionList :transactions="transactions"></TransactionList>
     <AddTransaction></AddTransaction>
